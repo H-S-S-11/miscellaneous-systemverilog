00cc24f729108132896d32fb0ce159e6b2419840
H-S-S-11 <harry@snell.org.uk>
2020-05-04 19:47:16 +0100

    reset synchroniser

---
+++ reset_synchroniser/reset_synchroniser.sv
@@ -0,0 +1,26 @@
+//Converts an async reset into a sync reset lasting one clock pulse
+
+module reset_sync #(
+    INPUT_POLARITY=0,   //Active low
+    OUTPUT_POLARITY=1   //Active high
+)(
+    input logic async_reset_i, clk,
+    output logic sync_reset_o
+);
+    
+logic [2:0] reset_sync;
+logic async_reset;
+assign async_reset = async_reset_i^INPUT_POLARITY;
+
+always_ff @(posedge clk, negedge async_reset) begin
+    if(~async_reset) begin
+        reset_sync[0] <= 1'b0;
+    end else begin
+        reset_sync <= {reset_sync[1:0], 1'b1};
+    end
+    
+end
+
+assign sync_reset_o = reset_sync[2]^OUTPUT_POLARITY;
+
+endmodule
---
+++ reset_synchroniser/test_reset_synchroniser.sv
@@ -0,0 +1,27 @@
+//Testing reset_sync
+
+module test_reset_sync();
+
+logic clk, async_reset, sync_reset;
+    
+reset_sync #(
+    .INPUT_POLARITY(0),
+    .OUTPUT_POLARITY(1)
+)   sync_1(
+    .clk(clk),
+    .async_reset_i(async_reset),
+    .sync_reset_o(sync_reset)
+);
+
+initial begin
+    clk = 0;
+    async_reset = 1;
+    #10ns async_reset = 0;
+    #10ns async_reset = 1;
+end
+
+always begin
+    #5ns clk = ~clk;
+end
+
+endmodule
